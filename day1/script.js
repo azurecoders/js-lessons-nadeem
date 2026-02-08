96020dd86a9b134ecfea788dafced91d3ee344d8
azurecoders <azfarchannel54321@gmail.com>
2026-02-08 12:44:50 +0500

    Day 1 of Javascript

---
+++ day1/script.js
@@ -0,0 +1,157 @@
+// Client-Side Language
+// Server-Side Language
+
+// console.log("This is my first message");
+
+// DRY - DO NOT REPEAT YOURSELF
+
+// Variables  -> Empty Container in which we store particular type of data
+
+// var name = "Muzammil"; // Normal Case
+// var myName = "Muzammil"; // camelCase
+// lastname
+// firstName
+
+// namethatwillbeusedforauthentication
+// nameThatWillBeUsedForAuthentication
+
+// var my_name = "Ahmed"; // Snake Case
+// var PascalCase = "Nadeem"; // Pascal Case
+
+// var myName = "Muzammil";
+
+// console.log(myName);
+
+// var myName = "Ahmed";
+
+// console.log(myName);
+
+// var myName = "Muzammil";
+// console.log(myName);
+
+// myName = "Nadeem";
+// console.log(myName);
+
+// let myName = "Muzammil";
+
+// console.log(myName);
+// console.log(myName.toUpperCase());
+
+// myName = "Ahmed";
+// console.log(myName);
+
+// const myName = "Nadeem";
+// console.log(myName);
+
+// myName = "Ahmed";
+// console.log(myName);
+
+// Primitive Data Types
+
+// String
+// let myName = "muzammil"; // 'muzammil'
+// console.log(typeof myName, myName);
+// console.log(myName.toUpperCase()[0]);
+
+// Number -> Integer (Non - Decimal Point) - Float (Decimal Point Value)
+// let myAge = 21;
+// let myMarks = 87.9;
+
+// console.log(typeof myAge, myAge);
+// console.log(typeof myMarks, myMarks);
+
+// Boolean -> true and false
+
+// let isValid = true;
+// let isStudying = false;
+
+// console.log(typeof isValid, isValid);
+// console.log(typeof isStudying, isStudying);
+
+// Null -> Object
+
+// let myNewName = null;
+// console.log(typeof myNewName, myNewName);
+
+// Fetching Data from the backend
+
+// myNewName = "Muzammil";
+// console.log(typeof myNewName, myNewName);
+
+// Undefined
+
+// let myAgeFromBdend;
+// console.log(typeof myAgeFromBdend, myAgeFromBdend);
+
+// Operators
+
+// 1. Arithmetic Operator - Normal Math Operations
+
+// let a = 10;
+// let b = 20;
+
+// console.log("The ouput of a + b: ", a + b);
+// console.log("The ouput of a - b: ", a - b);
+// console.log("The ouput of a * b: ", a * b);
+// console.log("The ouput of a / b: ", a / b);
+// console.log("The ouput of a % b: ", a % b); // Mod Operator -> Returns remainder
+// console.log("The ouput of a ** b: ", a ** b); // Exponential Operator
+
+// 2. Assignment Operators
+
+// let c = 20; // Variable Declaration
+// c += b;
+// c -= b;
+// c *= b;
+// c /= b;
+// c %= b;
+
+// c++; // Double Increment Operator
+// c--; // Double Decrement Operator
+
+// c += 5;
+
+// console.log(c);
+
+// 3. Comaprison Operators
+
+// let a = 10;
+// let b = 20;
+
+// console.log("The output of a > b: ", a > b);
+// console.log("The output of a < b: ", a < b);
+// console.log("The output of a <= b: ", a <= b);
+// console.log("The output of a >= b: ", a >= b);
+// console.log("The output of a == b: ", a == b);
+// console.log("The output of a != b: ", a != b);
+
+// let i = "10";
+// let j = 10;
+
+// console.log("The ouput of i == j: ", i == j);
+// console.log("The ouput of i === j: ", i === j); // Strict Equality
+
+// 4. Logical Operators -> Deals with Boolean or Truthy and Falsy Values
+
+// Truth - 1 or String Value
+// False - 0
+
+// AND
+
+// let a = 0;
+// let b = 1;
+
+// console.log("AND Operator: ", a && b);
+
+// OR
+
+// let a = false;
+// let b = false;
+
+// console.log("OR Operator: ", a || b);
+
+// NOT
+
+// let a = true;
+
+// console.log("NOT Operator: ", !a);
